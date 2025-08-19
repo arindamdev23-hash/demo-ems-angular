39302da82319a81239cade4bcc69b1cd81c48c94
Arindam Samanta <arindam.samanta@health.telstra.com>
2025-08-19 19:03:58 +0530

    route configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layout/header/header.component';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, MainLayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { EmployeeListComponent } from './features/employees/employee-list/employee-list.component';
+import { EmployeeDetailsComponent } from './features/employees/employee-details/employee-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'employees', component: EmployeeListComponent },
+  { path: 'employees/:id', component: EmployeeDetailsComponent },
+];
